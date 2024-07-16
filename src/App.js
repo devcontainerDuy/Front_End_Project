a95634331522c55bea4d83f5c31485dd5cfd2b22
Dom <leodomsolar@gmail.com>
2024-07-16 18:46:40 +0700

    Commit

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ function App() {
         <Route path='/dang-nhap' element={<Login/>}/>
         <Route path='/tim-kiem/:id' element={<Search/>}/>
         <Route path='/not-found'  element={<NotFound/>}/>
+        <Route path='/pay-success'  element={<NotFound/>}/>
         <Route path='*'  element={<NotFound/>}/>
     
       </Routes>
