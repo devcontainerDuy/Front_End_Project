79f393a2ce2b4f15e1a2fda68ba6588db99dfc31
Dom <dev@trendi.ai>
2024-09-13 22:09:02 +0700

    Commit

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,44 +1,79 @@
-import { useState } from "react";
-import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
-import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from "@chatscope/chat-ui-kit-react";
-import runChat from "../config/gemini";
+/* eslint-disable*/
+import { useState, useEffect }  from 'react';
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+} from '@chatscope/chat-ui-kit-react';
+
+const API_KEY =process.env.REACT_APP_OPEN_AI_KEY
 
 const Chat = () => {
-	const [messages, setMessages] = useState([
-		{
-			message: "Chào bạn, tôi là AI . Tôi có thể giúp gì bạn á !",
-			sentTime: "just now",
-			sender: "GeminiAI",
-			position: "single",
-			direction: "incoming",
-		},
-	]);
-	const [isTyping, setIsTyping] = useState(false);
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I'm ChatGPT! Ask me anything!",
+      sentTime: "just now",
+      sender: "ChatGPT",
+    },
+  ]);
+  const [isTyping, setIsTyping] = useState(false);
 
-	const handleSendRequest = async (message) => {
-		const newMessage = {
-			message,
-			direction: "outgoing",
-			sender: "user",
-			position: "first",
-		};
+  const handleSendRequest = async (message) => {
+    const newMessage = {
+      message,
+      direction: 'outgoing',
+      sender: "user",
+    };
 
-		setMessages((prevMessages) => [...prevMessages, newMessage]);
-		setIsTyping(true);
+    setMessages((prevMessages) => [...prevMessages, newMessage]);
+    setIsTyping(true);
 
-		try {
-			const response = await runChat(message); // Call the runChat function
-			const geminiAIResponse = {
-				message: response,
-				sender: "GeminiAI",
-			};
-			setMessages((prevMessages) => [...prevMessages, geminiAIResponse]);
-		} catch (error) {
-			console.error("Error processing message:", error);
-		} finally {
-			setIsTyping(false);
-		}
-	};
+    try {
+      const response = await processMessageToChatGPT([...messages, newMessage]);
+      const content = response.choices[0]?.message?.content;
+      if (content) {
+        const chatGPTResponse = {
+          message: content,
+          sender: "ChatGPT",
+        };
+        setMessages((prevMessages) => [...prevMessages, chatGPTResponse]);
+      }
+    } catch (error) {
+      console.error("Error processing message:", error);
+    } finally {
+      setIsTyping(false);
+    }
+  };
+
+  async function processMessageToChatGPT(chatMessages) {
+    const apiMessages = chatMessages.map((messageObject) => {
+      const role = messageObject.sender === "ChatGPT" ? "assistant" : "user";
+      return { role, content: messageObject.message };
+    });
+
+    const apiRequestBody = {
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        { role: "system", content: "I'm a Student using ChatGPT for learning" },
+        ...apiMessages,
+      ],
+    };
+
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(apiRequestBody),
+    });
+
+    return response.json();
+  }
 
 	return (
 		<div className="App">
@@ -46,7 +81,7 @@ const Chat = () => {
 				<div className="col position-relative" style={{ height: "360px" }}>
 					<MainContainer>
 						<ChatContainer>
-							<MessageList scrollBehavior="smooth" typingIndicator={isTyping ? <TypingIndicator content="Gemini AI đang soạn tin nhắn..." /> : null}>
+							<MessageList scrollBehavior="smooth" typingIndicator={isTyping ? <TypingIndicator content="GPT đang soạn tin nhắn..." /> : null}>
 								{messages.map((message, i) => (
 									<Message
 										key={i}
