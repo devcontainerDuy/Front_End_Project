c02cd12ddf56cdc7fbe5504f74cfe2fb41418331
Dom <dev@trendi.ai>
2024-09-15 09:16:15 +0700

    Commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { store } from "./store";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <GoogleOAuthProvider clientId={process.env.REACT_APP_GG_KEY}>
   <Provider store={store}>
   <App/>
   </Provider>
+  </GoogleOAuthProvider>
 
 );
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,67 @@
 /* eslint-disable*/
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Notyf } from "notyf";
 import "notyf/notyf.min.css";
 import axios from "axios";
 import { Helmet } from "react-helmet";
-
+import { googleLogout, useGoogleLogin } from '@react-oauth/google';
 function Login() {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const [ user, setUser ] = useState([]);
+    const [ profile, setProfile ] = useState([]);
+
+    const login = useGoogleLogin({
+        onSuccess: (codeResponse) => setUser(codeResponse),
+        onError: (error) => console.log('Login Failed:', error)
+    });
+    useEffect(
+        () => {
+            if (user) {
+                axios
+                    .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+                        headers: {
+                            Authorization: `Bearer ${user.access_token}`,
+                            Accept: 'application/json'
+                        }
+                    })
+                    .then((res) => {
+                        setProfile(res.data);
+                    })
+                    .catch((err) => console.log(err));
+            }
+        },
+        [ user ]
+    );
+
+    // log out function to log the user out of google and set the profile array to null
+    const logOut = () => {
+        googleLogout();
+        setProfile(null);
+    };
+	useEffect(() => {
+		if(profile.email){
+			axios
+				.post(process.env.REACT_APP_API_URL + "customers/auth/login-email", {
+					email: profile.email,
+				})
+				.then((res) => {
+					if(res.data.check == true){
+						localStorage.setItem("token", res.data.token);
+						localStorage.setItem("id", res.data.id);
+						notyf.open({
+							type: "success",
+							message: "Đăng nhập thành công",
+						});
+						setTimeout(() => {
+							window.location.replace("/");
+						}, 1200);
+					}
+				});
+		}
+	}, [profile]);
 	const notyf = new Notyf({
 		duration: 1000,
 		position: {
@@ -158,7 +210,7 @@ function Login() {
 												<div className="col-12">
 													<div className="d-grid">
 														<button className="btn bsb-btn-xl btn-dark" onClick={(e) => submitLogin()} type="button">
-															Log in now
+															Đăng nhập
 														</button>
 													</div>
 												</div>
@@ -181,13 +233,13 @@ function Login() {
 											<div className="col-12">
 												<p className="mt-5 mb-4">Hoặc đăng nhập với</p>
 												<div className="d-flex gap-3 flex-column flex-xl-row">
-													<a href="#!" className="btn bsb-btn-xl btn-outline-dark">
+													<a href="#!" onClick={login} className="btn bsb-btn-xl btn-outline-dark">
 														<svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="currentColor" className="bi bi-google" viewBox="0 0 16 16">
 															<path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
 														</svg>
 														<span className="ms-2 fs-6">Google</span>
 													</a>
-													<a href="#!" className="btn bsb-btn-xl btn-outline-dark">
+													{/* <a href="#!" className="btn bsb-btn-xl btn-outline-dark">
 														<svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="currentColor" className="bi bi-facebook" viewBox="0 0 16 16">
 															<path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
 														</svg>
@@ -198,7 +250,7 @@ function Login() {
 															<path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
 														</svg>
 														<span className="ms-2 fs-6">Twitter</span>
-													</a>
+													</a> */}
 												</div>
 											</div>
 										</div>
